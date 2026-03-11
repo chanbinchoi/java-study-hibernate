c8093739bd07c2aa40676840e35dfdbdc551cea7
Chanbin Choi <classy9611@gmail.com>
2026-03-11 17:57:23 +0900

    ManyToMany

---
+++ src/main/java/com/telusko/Alien.java
@@ -0,0 +1,60 @@
+package com.telusko;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+@Entity // 이 객체를 DB 테이블의 한 행(row)과 연결해서 사용할 수 있게 만듦
+public class Alien {
+
+    @Id // 기본키 필드
+    private int aid;
+    private String aname;
+    private String tech;
+    @ManyToMany
+    private List<Laptop> laptops;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public List<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    public void setLaptops(List<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
+                ", laptop=" + laptops +
+                '}';
+    }
+}
