a15fb4b1f874a437dd4e7a0b149a5eb573577817
chanbinchoi <classy9611@gmail.com>
2026-03-12 22:53:28 +0900

    Hibernate end

--- src/main/java/com/telusko/Laptop.java
+++ src/main/java/com/telusko/Laptop.java
@@ -1,9 +1,11 @@
 package com.telusko;
 
+import jakarta.persistence.Cacheable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
+@Cacheable
 public class Laptop {
 
     @Id
--- src/main/java/com/telusko/Main.java
+++ src/main/java/com/telusko/Main.java
@@ -3,9 +3,6 @@ package com.telusko;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,24 +14,15 @@ public class Main {
 
         Session session = sf.openSession();
 
-        // select * from laptop where ram=32 -> SQL
-        // from Laptop where ram=32
-
-        String brand = "Asus";
-
-        Query query = session.createQuery("select model from Laptop where brand like ?1");
-        query.setParameter(1, brand);
-        List<Object[]> laptops = query.getResultList();
-
-//        Laptop l1 = session.get(Laptop.class, 3);
-
-        for (Object[] data : laptops) {
-            System.out.println((String) data[0] + " " + (String) data[1]);
-        }
-
-        System.out.println(laptops);
+        Laptop l1 = session.get(Laptop.class, 2);
+        System.out.println(l1);
 
         session.close();
+
+        Session session1 = sf.openSession();
+
+        Laptop l2 = session.get(Laptop.class, 2);
+        System.out.println(l2);
 
         sf.close();
 
